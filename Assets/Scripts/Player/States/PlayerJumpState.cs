a2e6f7be39c71ab07959081b1d8f439e82424692
Max <qbite0@gmail.com>
2022-08-08 21:56:22 +0700

    many indusian

--- Assets/Scripts/Player/States/PlayerJumpState.cs
+++ Assets/Scripts/Player/States/PlayerJumpState.cs
@@ -17,6 +17,25 @@ class PlayerJumpState : StateBase<PlayerStates> {
         
         _controller.StartCoroutine(ResetJump(_controller.JumpCooldown));
     }
+    
+    public override void OnLogic() {
+        MovePlayer();
+        SpeedControl();
+    }
+    
+    private void MovePlayer() {
+        Vector3 _moveDirection = _controller.Orientation.forward * _controller.Input.MovementDirection.y + _controller.Orientation.right * _controller.Input.MovementDirection.x;
+        _controller.RigidBody.AddForce(_moveDirection.normalized * _controller.DesiredMoveSpeed * 10f * _controller.AirMultiplier, ForceMode.Force);
+    }
+    
+    private void SpeedControl() {
+        Vector3 flatVel = new Vector3(_controller.RigidBody.velocity.x, 0f, _controller.RigidBody.velocity.z);
+
+        if (flatVel.magnitude > _controller.DesiredMoveSpeed) {
+            Vector3 limitedVel = flatVel.normalized * _controller.DesiredMoveSpeed;
+            _controller.RigidBody.velocity = new Vector3(limitedVel.x, _controller.RigidBody.velocity.y, limitedVel.z);
+        }
+    }
 
     private void Jump() {
         _controller.RigidBody.velocity = new Vector3(_controller.RigidBody.velocity.x, 0f, _controller.RigidBody.velocity.z);
