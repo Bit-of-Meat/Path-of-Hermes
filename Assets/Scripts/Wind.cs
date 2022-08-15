6e401afd9698ba1d64a8bc92bbfe537fdbc9ed5e
Nikita Arhipov <61907599+BeshkekArt@users.noreply.github.com>
2022-08-15 16:00:50 +0400

    churkapoketedition

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -3,6 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wind : MonoBehaviour {
-    public float strength;
-    public Vector3 direction;
+    [SerializeField] private float strength;
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private LayerMask _windLayerMask;
+
+    [SerializeField] private bool _inWindZone = false;
+
+    private void FixedUpdate() { 
+        if (_inWindZone) _rigidbody.AddForce(direction * strength);
+    }
+
+    void OnTriggerEnter(Collider coll) {
+        if (_windLayerMask == 1 << coll.gameObject.layer)
+            _inWindZone = true;
+    }
+
+    void OnTriggerExit(Collider coll) {
+        if (_windLayerMask == 1 << coll.gameObject.layer)
+            _inWindZone = false;
+    }
 }
