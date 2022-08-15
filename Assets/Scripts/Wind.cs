d9551e52c4051a433f46ee81d8366d85ac161715
Max <qbite0@gmail.com>
2022-08-15 20:06:41 +0700

    new shit

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -1,28 +1,24 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Wind : MonoBehaviour {
     [SerializeField] private float strength;
     [SerializeField] private Vector3 direction;
-    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Vector3 _size;
     [SerializeField] private LayerMask _windLayerMask;
 
-    [SerializeField] private bool _inWindZone = false;
-
     private void FixedUpdate() { 
-        if (_inWindZone) _rigidbody.AddForce(direction * strength);
+        var _colliders = Physics.OverlapBox(transform.position, _size, Quaternion.identity, _windLayerMask);
+        foreach (var _collider in _colliders)
+            if (_collider.attachedRigidbody != null) _collider.attachedRigidbody.AddForce(direction * strength);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, _size);
     }
 
     private bool CheckLayer(LayerMask layerMask, int layer) {
         return layerMask == (layerMask | (1 << layer));
-    }
-
-    private void OnTriggerEnter(Collider collider) {
-        if (CheckLayer(_windLayerMask, collider.gameObject.layer))
-            _inWindZone = true;
-    }
-
-    private void OnTriggerExit(Collider collider) {
-        if (CheckLayer(_windLayerMask, collider.gameObject.layer))
-            _inWindZone = false;
     }
 }
