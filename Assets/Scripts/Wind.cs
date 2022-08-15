64535197a7bb856217f619fd8425275965db4c45
Nikita Arhipov <61907599+BeshkekArt@users.noreply.github.com>
2022-08-15 19:27:45 +0400

    хз чё поменял уровень топ

--- Assets/Scripts/Wind.cs
+++ Assets/Scripts/Wind.cs
@@ -1,24 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wind : MonoBehaviour {
+public class Wind : MonoBehaviour
+{
     [SerializeField] private float strength;
     [SerializeField] private Vector3 direction;
     [SerializeField] private Vector3 _size;
     [SerializeField] private LayerMask _windLayerMask;
 
-    private void FixedUpdate() { 
+    private void FixedUpdate()
+    {
         var _colliders = Physics.OverlapBox(transform.position, _size, Quaternion.identity, _windLayerMask);
         foreach (var _collider in _colliders)
             if (_collider.attachedRigidbody != null) _collider.attachedRigidbody.AddForce(direction * strength);
     }
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos()
+    {
         Gizmos.color = Color.green;
         Gizmos.DrawCube(transform.position, _size);
     }
 
-    private bool CheckLayer(LayerMask layerMask, int layer) {
+    private bool CheckLayer(LayerMask layerMask, int layer)
+    {
         return layerMask == (layerMask | (1 << layer));
     }
 }
